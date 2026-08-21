48a03398be8d9a8816c02a3d060465fc38f466a3
dejede <77575449+dejede@users.noreply.github.com>
2026-08-21 21:14:43 +0700

    Add files via upload

---
+++ assets/vendors.js
@@ -0,0 +1,40 @@
+const vendorData = {
+  "decor": [
+    { "name": "Brontoseno", "ig": "@brontosenorusticdecoration" },
+    { "name": "Nafcraft", "ig": "@nafcraft_decoration" },
+    { "name": "AK", "ig": "@riidwan_ak" }
+  ],
+  "mua": [
+    { "name": "Qina Bride", "ig": "@qinabride" },
+    { "name": "Beauty Yoja", "ig": "@beauty_yoja" },
+    { "name": "Marchel Makeup", "ig": "@marchel_makeup" },
+    { "name": "Shangrilla", "ig": "@afrilia.istiaris" },
+    { "name": "Rimba Salon", "ig": "@rimbasalon_tempursari" },
+    { "name": "Salon Dewi", "ig": "@salonndewi" },
+    { "name": "Santi Linda", "ig": "@santilinda_wedding" }
+  ],
+  "henna": [
+    { "name": "Sisil", "ig": "@sisilliamonica" },
+    { "name": "Zeera", "ig": "@zeera_amoora" }
+  ],
+  "hairdoo": [
+    { "name": "Mawar Hairdo", "ig": "@mawarmua_hairdo" },
+    { "name": "Mining Hairstyle", "ig": "@fairusbey" }
+  ],
+  "mc": [
+    { "name": "Mbero", "ig": "@prena.prena.3591" },
+    { "name": "Rama", "ig": "@ramafadholi.official" },
+    { "name": "Syahroni", "ig": "@ahmadsyahroni" }
+  ],
+  "support": [
+    { "name": "Budi", "ig": "@rafaelbudhy" },
+    { "name": "LGR Production", "ig": "@lgrproduction" },
+    { "name": "AK", "ig": "@riidwan_ak" },
+    { "name": "Hanti MDS", "ig": "@hanti_mds_tendaweddinglumajang" }
+  ],
+  "entertainment": [
+    { "name": "Meganza", "ig": "@meganza" },
+    { "name": "Ari Project", "ig": "@ariproject" },
+    { "name": "Band", "ig": "@adaband" }
+  ]
+};
